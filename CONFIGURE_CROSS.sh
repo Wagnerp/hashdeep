c81597603aa53565e3b84702d7f224a934977020
Jesse Kornblum <jessekornblum@gmail.com>
2013-07-24 09:12:10 -0700

    Updating cross compile script for wget

--- CONFIGURE_CROSS.sh
+++ CONFIGURE_CROSS.sh
@@ -12,21 +12,24 @@ the Mingw-w64 compilers. It requires:
 
 2. This script. Put it in your home directory.
 
-3. You will be prompted for your password during the process.
+3. The program 'wget'. You can install this program via MacPorts.
+
+4. You will be prompted for your password during the process.
 
 press any key to continue...
 EOF
 read
 
-#if [ $USER != "root" ]; then
-#  echo ERROR: You must run this script as root.
-#  exit 1
-#fi
-
 if [ ! `uname` == 'Darwin' ]; then
   echo ERROR: This script is intended for OS X systems.
   echo Once you go Mac, you never go back...
   exit 1
+fi
+
+if [[ ! -x `which wget` ]]; then
+    echo ERROR: You must have wget installed to use this script.
+    echo The easiest way to get wget is through MacPorts, http://macports.org/
+    exit 1
 fi
 
 echo Ready to install.
@@ -39,18 +42,18 @@ I686_DATE=20130531
 I686_FILENAME=mingw-w32-bin_i686-darwin_$I686_DATE.tar.bz2
 if [ ! -r $I686_FILENAME ]; then
     echo Downloading 32-bit cross compiler
-    curl -L "http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Automated%20Builds/mingw-w32-bin_i686-darwin_$I686_DATE.tar.bz2/download" > $I686_FILENAME
+    wget "http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Automated%20Builds/mingw-w32-bin_i686-darwin_$I686_DATE.tar.bz2/download" > $I686_FILENAME
 fi
 
 X64_DATE=20130615
 X64_FILENAME=mingw-w64-bin_i686-darwin_$X64_DATE.tar.bz2
 if [ ! -r $X64_FILENAME ]; then
     echo Downloading 64-bit cross compiler
-    curl -L "http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Automated%20Builds/mingw-w64-bin_i686-darwin_$X64_DATE.tar.bz2/download" > $X64_FILENAME
+    wget "http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Automated%20Builds/mingw-w64-bin_i686-darwin_$X64_DATE.tar.bz2/download" > $X64_FILENAME
 fi
 
 echo
-echo Expanding files
+echo Expanding cross compilers...
 echo
 rm -rf i686-w64-mingw32 x86_64-w64-mingw32
 mkdir i686-w64-mingw32 x86_64-w64-mingw32
@@ -61,7 +64,7 @@ tar jxf ../$X64_FILENAME
 cd ..
 
 echo
-echo Moving files into position. Please enter your password at the prompt:
+echo Installing cross compilers. Please enter your password at the prompt:
 echo
 sudo mkdir -p /usr/local
 sudo mv i686-w64-mingw32 x86_64-w64-mingw32 /usr/local
@@ -101,6 +104,19 @@ echo Successfully installed pthreads.
 
 ## RBF - Add new compilers to $PATH in .profile
 ## RBF - Do we need to update/remove the compiler flags for cross compilation?
+
+echo
+echo Adding cross compilers to PATH environment variable
+echo
+for CROSS in i686-w64-mingw32 x86_64-w64-mingw32
+do
+    if [[ echo $PATH | grep /usr/local/$CROSS/bin -eq 1 ]]; then
+	echo ADDING $CROSS
+    fi
+done
+
+
+
 
 
 echo ================================================================
